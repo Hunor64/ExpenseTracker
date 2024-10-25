825501e589177107be86e5c097392c0c21e401a2
hunor64 <nhunor06@gmail.com>
2024-10-25 13:44:24 +0200

    Code update

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
-import AddTransaction from './components/AddTransaction';
-import Balance from './components/Balance';
-import Header from './components/Header';
-import TransactionList from './components/TransactionList';
+import AddTransaction from "./components/AddTransaction"
+import Balance from "./components/Balance"
+import Header from "./components/Header"
+import TransactionList from "./components/TransactionList"
+import './App.css'
+import IncomeExpense from "./components/IncomeExpense"
 
 const App = () => {
     return (
         <>
-            <Header/>
-            <Balance/>
-            <TransactionList/>
-            <AddTransaction/>
+            <Header />
+            <Balance />
+            <IncomeExpense />
+            <TransactionList />
+            <AddTransaction />
         </>
-    );
+    )
 }
 
-export default App;
+export default App
--- src/components/Balance.tsx
+++ src/components/Balance.tsx
@@ -2,17 +2,7 @@ const Balance = () => {
     return (
         <div>
             <h3>Your Balance</h3>
-            <div className="balanceContainer">
-                <h1 className="">$0.00</h1>
-                <div className="income">
-                    <h4>Income</h4>
-                    <p>$0.00</p>
-                </div>
-                <div className="expense">
-                    <h4>Expense</h4>
-                    <p>$0.00</p>
-                </div>
-            </div>
+            <h1>$0.00</h1>
         </div>
     )
 }
