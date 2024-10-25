88be50b0a1c34a0183b7d92484c36706c1ca9c3c
hunor64 <nhunor06@gmail.com>
2024-10-25 13:30:51 +0200

    Added new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import Balance from './components/Balance';
+import Header from './components/Header';
+import TransactionList from './components/TransactionList';
+
 const App = () => {
-  return (
-    <div>App</div>
-  )
+    return (
+        <>
+            <Header/>
+            <Balance/>
+            <TransactionList/>
+        </>
+    );
 }
 
-export default App
+export default App;
---
+++ src/components/TransactionList.tsx
@@ -0,0 +1,14 @@
+const TransactionList = () => {
+    return (
+        <div>
+            <h3>History</h3>
+            <ul className="list">
+                <li className="expense">
+                    Flowers <span>-$30</span>
+                    <button className="delete">X</button>
+                </li>
+            </ul>
+        </div>
+    )
+}
+export default TransactionList
